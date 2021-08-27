c0f5ebace4181048258b2512766d546e5f45dcae
Baddam Akhila <Baddam Akhila@LAPTOP-0C57MPBI>
2021-08-27 07:36:12 +0530

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                /* //array declaration
+                int[] arr = new int[6] ;
+                Console.WriteLine("enter array elements");
+                for (int i= 0;i< arr.Length;i++)
+                {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+
+                 }
+
+                //linq methods
+                //Console.WriteLine( arr.Max());
+
+                //array methods
+                //Array.Sort(arr);
+                foreach (int i in arr)
+                {
+                Console.WriteLine(i);
+                }
+                }*/
+
+                //two dimensional array
+
+                int[,] arr2d = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+                for (int i = 0; i < arr2d.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arr2d.GetLength(1); j++)
+                    {
+                        Console.Write("\t{0}", arr2d[i, j]);
+                    }
+                    Console.WriteLine("\n");
+                }
+
+            }
+        }
+    }
+}
+
+   
---
+++ Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string str;
+         Console.WriteLine("enter a string");
+         str = Console.ReadLine();
+         Console.WriteLine("string is :{​​​0}​​​",str);*/
+            int a = 10, b = 20, c;
+            /* Console.WriteLine("enter a Value");
+            a = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("enter b value");
+             b = Convert.ToInt32(Console.ReadLine());*/
+            c = a + b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
+   
